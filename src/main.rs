e4d555071b631d4dcfd4d838de425f2ac6879e4a
da_louis <ruaces@gmail.com>
2020-08-26 22:44:56 +0900

    receive command line parameter via clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Clap;
+
+#[derive(Clap,Debug)]
+#[clap(
+    name = "My RPN program :)",
+    version = "1.0.0",
+    author = "da-louis",
+    about = "Super awesome fantastic happy great highest sample RPN calculator :doge:"
+)]
+struct Opts{
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose:bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file:Option<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    match opts.formula_file{
+        Some(file)=> println!("File specified: {}", file),
+        None=>println!("No file specified")
+    }
+
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
