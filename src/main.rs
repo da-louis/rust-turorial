fc3102466c9833d159ee6a687a3daac3e49f8c8e
da_louis <ruaces@gmail.com>
2020-08-26 22:51:05 +0900

    code formatted.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
 use clap::Clap;
 
-#[derive(Clap,Debug)]
+#[derive(Clap, Debug)]
 #[clap(
-    name = "My RPN program :)",
-    version = "1.0.0",
-    author = "da-louis",
-    about = "Super awesome fantastic happy great highest sample RPN calculator :doge:"
+name = "My RPN program :)",
+version = "1.0.0",
+author = "da-louis",
+about = "Super awesome fantastic happy great highest sample RPN calculator :doge:"
 )]
-struct Opts{
+struct Opts {
     /// Sets the level of verbosity
     #[clap(short, long)]
-    verbose:bool,
+    verbose: bool,
 
     /// Formulas written in RPN
     #[clap(name = "FILE")]
-    formula_file:Option<String>
+    formula_file: Option<String>,
 }
 
 fn main() {
     let opts = Opts::parse();
 
-    match opts.formula_file{
-        Some(file)=> println!("File specified: {}", file),
-        None=>println!("No file specified")
+    match opts.formula_file {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified")
     }
 
     println!("Is verbosity specified?: {}", opts.verbose);
