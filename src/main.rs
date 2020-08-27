136580d9d0805d4582db2a66ecadea42c7b83ed5
da_louis <ruaces@gmail.com>
2020-08-28 01:08:53 +0900

    revert :/

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,6 @@ use std::io::{BufRead, BufReader, stdin};
 
 use clap::Clap;
 
-#[derive(Clap, Debug)]
-#[clap(
-name = "My RPN program :)",
-version = "1.0.0",
-author = "da-louis",
-about = "Super awesome fantastic happy great highest sample RPN calculator :doge:"
-)]
-struct Opts {
-    /// Sets the level of verbosity
-    #[clap(short, long)]
-    verbose: bool,
-
-    /// Formulas written in RPN
-    #[clap(name = "FILE")]
-    formula_file: Option<String>,
-}
-
 struct RpnCalculator(bool);
 
 impl RpnCalculator {
@@ -63,6 +46,23 @@ impl RpnCalculator {
             panic!("invalid syntax")
         }
     }
+}
+
+#[derive(Clap, Debug)]
+#[clap(
+name = "My RPN program :)",
+version = "1.0.0",
+author = "da-louis",
+about = "Super awesome fantastic happy great highest sample RPN calculator :doge:"
+)]
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
 }
 
 fn main() {
